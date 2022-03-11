0068144922bb4a7b213911dd0595dedbd3eebea0
Thomas Thirion <thirionthomas68100@gmail.com>
2022-03-11 11:53:50 +0100

    modif page item

--- pages/blog/items.js
+++ pages/blog/items.js
@@ -5,6 +5,16 @@ export default function Items() {
   return (
     <Layout>
     <h1>Agence immo-sous répertoire, Items</h1>
+    <ul>
+      <h2>Liste des Catégories</h2>
+      <li>Localisation</li>
+      <li>Prix</li>
+      <li>Superficie</li>
+      <li>Jardin</li>
+    </ul>
     </Layout>
   )
 }
+
+// exemple de mise a jour de page lorsque l 'application est deja deployée sur vercel
+// nous rajoutons une liste
